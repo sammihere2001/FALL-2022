30c3f97d65d2a866ecf48bb8e1fdd55a8e14d06a
sammihere2001 <117255703+sammihere2001@users.noreply.github.com>
2023-01-02 12:57:10 +0500

    add new file

---
+++ assgiment 1/problem 9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            //Problem  9
+
+             Console.WriteLine("Number of Rows you want to print the pattern ");
+             int n = Convert.ToInt32(Console.ReadLine());
+
+             for (int i = 0; i <= n; i++)
+             {
+                 for (int j = 0; j < i; j++)
+                 {
+                     Console.Write("*");
+                 }
+                 Console.WriteLine();
+             }
+             Console.ReadKey();
+        }
+    }
+}
